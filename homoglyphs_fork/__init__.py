f5e20d05abc91a83972ad97050f2cb23ca87a6f8
Matt Copperwaite <618054+yamatt@users.noreply.github.com>
2025-12-30 14:25:14 +0000

    lint fix

--- homoglyphs_fork/__init__.py
+++ homoglyphs_fork/__init__.py
@@ -8,12 +8,16 @@ Homoglyphs
 """
 
 # main package info
-__title__ = 'Homoglyphs'
-__author__ = 'Gram Orsinium'
-__license__ = 'MIT'
+__title__ = "Homoglyphs"
+__author__ = "Gram Orsinium"
+__license__ = "MIT"
 
 
-from .core import ( # noQA
-    Categories, Languages, Homoglyphs,
-    STRATEGY_LOAD, STRATEGY_IGNORE, STRATEGY_REMOVE
+from .core import (  # noQA
+    Categories,
+    Languages,
+    Homoglyphs,
+    STRATEGY_LOAD,
+    STRATEGY_IGNORE,
+    STRATEGY_REMOVE,
 )
